51397f89ab8402c6049c9729ada84c2060e95061
Emma Gilhuly <emmagilhuly@me.com>
2016-01-09 14:16:28 -0800

    amazon s3

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -7,12 +7,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
   # storage :fog
 
-  def cache_dir
-    "#{Rails.root}/tmp/uploads"
-  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
